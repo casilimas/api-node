a2a811bbfb337528852ab1935249601cce6b6bac
freddy andres casilimas sanchez <tu_correo@example.com>
2024-06-08 10:08:46 -0400

    Añadido controlador crear y modificaciones necesarias en archivos del primer commit

--- src/controllers/routers/rutas.js
+++ src/controllers/routers/rutas.js
@@ -1,9 +1,12 @@
-// src/routers/rutas.js
-
 const express = require('express');
 const router = express.Router();
+const userController = require('../../controllers/crear');  
 
-// Definir tus rutas 
+
+// Ruta para crear un nuevo usuario
+router.post('/api/crear', userController.createUser);
+
+// Ruta de prueba para verificar que el servidor está funcionando
 router.get('/', (req, res) => {
   res.send('Hello World');
 });
