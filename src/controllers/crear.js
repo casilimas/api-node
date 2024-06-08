1bf83051ddb5ad5859225014c1fbe5c990819695
freddy andres casilimas sanchez <tu_correo@example.com>
2024-06-08 11:43:56 -0400

    se agregan cambios a controlador crear, en base a la logica las validaciones

--- src/controllers/crear.js
+++ src/controllers/crear.js
@@ -2,18 +2,44 @@
 
 
 
-const User = require('../controllers/models/esquemas'); 
-
-
-
-
+const User = require('../../src/controllers/models/esquemas'); 
+const { emailValidator, nameValidator, idValidator } = require('../../src/controllers/helpers/validacion');
 
 // Crear nuevos usuarios
 const createUser = async (req, res) => {
+  const { nombres, apellidos, id, correo, profesion } = req.body;
+
+  // Validaciones
+  if (!nameValidator(nombres)) {
+    return res.status(400).json({ message: 'Nombres deben contener solo letras y tener un máximo de 50 caracteres.' });
+  }
+
+  if (!nameValidator(apellidos)) {
+    return res.status(400).json({ message: 'Apellidos deben contener solo letras y tener un máximo de 50 caracteres.' });
+  }
+
+  if (!idValidator(id)) {
+    return res.status(400).json({ message: 'ID debe contener solo números y tener un máximo de 11 caracteres.' });
+  }
+
+  if (!emailValidator(correo)) {
+    return res.status(400).json({ message: 'Correo debe ser una dirección válida de @hotmail.com.' });
+  }
+
   try {
-    const user = new User(req.body);
-    await user.save();
-    res.status(201).send(user);
+    const existingUserById = await User.findOne({ id });
+    if (existingUserById) {
+      return res.status(400).json({ message: 'ID ya existe.' });
+    }
+
+    const existingUserByEmail = await User.findOne({ correo });
+    if (existingUserByEmail) {
+      return res.status(400).json({ message: 'Correo ya existe.' });
+    }
+
+    const newUser = new User({ nombres, apellidos, id, correo, profesion });
+    await newUser.save();
+    res.status(201).send(newUser);
   } catch (error) {
     res.status(400).send(error);
   }
@@ -22,3 +48,4 @@ const createUser = async (req, res) => {
 module.exports = {
   createUser
 };
+
