e24a0386a97e3517d6f176865f07d170ca688e41
freddy andres casilimas sanchez <tu_correo@example.com>
2024-06-09 16:25:14 -0400

    Actualizar validadores para correos electrónicos y nombres/apellidos

--- src/controllers/helpers/validacion.js
+++ src/controllers/helpers/validacion.js
@@ -1,26 +1,23 @@
-
-
-
-
-
 const emailValidator = (email) => {
-    const regex = /^[a-zA-Z0-9._%+-]+@hotmail\.com$/;
-    return regex.test(email);
-  };
-  
-  const nameValidator = (name) => {
-    const regex = /^[a-zA-Z]{1,50}$/; // Solo letras, máximo 50 caracteres
-    return regex.test(name);
-  };
-  
-  const idValidator = (id) => {
-    const regex = /^[0-9]{1,11}$/;
-    return regex.test(id);
-  };
-  
-  module.exports = {
-    emailValidator,
-    nameValidator,
-    idValidator
-  };
-  
+  // Aceptar cualquier proveedor de correo electrónico
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(email);
+};
+
+const nameValidator = (name) => {
+  // Permitir letras y espacios, hasta 50 caracteres
+  const regex = /^[a-zA-Z\s]{1,50}$/;
+  return regex.test(name);
+};
+
+const idValidator = (id) => {
+  // Validar ID numérico de 1 a 11 dígitos
+  const regex = /^[0-9]{1,11}$/;
+  return regex.test(id);
+};
+
+module.exports = {
+  emailValidator,
+  nameValidator,
+  idValidator
+};
