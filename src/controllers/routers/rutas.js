752dce22866649bcc57545f12293465c22fc2c4c
freddy andres casilimas sanchez <tu_correo@example.com>
2024-06-09 14:51:02 -0400

    Guardando controlador buscar = cambios necesarios

--- src/controllers/routers/rutas.js
+++ src/controllers/routers/rutas.js
@@ -4,12 +4,14 @@ const userController = require('../crear');
 const { modificarUsuario } = require('../editar');
 const { listarUsuarios } = require('../listar');
 const { borrarUsuario } = require('../borrar');
+const { buscarUsuario } = require('../buscar'); 
 
 // Ruta para crear un nuevo usuario
 router.post('/api/crear', userController.createUser);
 router.put('/api/modificar/:id', modificarUsuario);
 router.get('/api/listar', listarUsuarios);
 router.delete('/api/borrar/:id', borrarUsuario);
+router.get('/api/buscar/:id', buscarUsuario);
 
 
 
