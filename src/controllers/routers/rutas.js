6abd819fbb8d08715d1c79a84ba7bf672e221153
freddy andres casilimas sanchez <tu_correo@example.com>
2024-06-09 12:01:00 -0400

    rama listar se genero controlador listar + cambios en rutas

--- src/controllers/routers/rutas.js
+++ src/controllers/routers/rutas.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../crear'); 
 const { modificarUsuario } = require('../editar');
+const { listarUsuarios } = require('../listar');
 
 // Ruta para crear un nuevo usuario
 router.post('/api/crear', userController.createUser);
 router.put('/api/modificar/:id', modificarUsuario);
+router.get('/api/listar', listarUsuarios);
 
 
 
