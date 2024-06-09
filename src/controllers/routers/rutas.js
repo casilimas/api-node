c10421e0540a16a6f0a6d42d824d307f96e3c5ca
freddy andres casilimas sanchez <tu_correo@example.com>
2024-06-09 14:26:23 -0400

    Guardando controlador borrar y rutas

--- src/controllers/routers/rutas.js
+++ src/controllers/routers/rutas.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const userController = require('../crear'); 
 const { modificarUsuario } = require('../editar');
 const { listarUsuarios } = require('../listar');
+const { borrarUsuario } = require('../borrar');
 
 // Ruta para crear un nuevo usuario
 router.post('/api/crear', userController.createUser);
 router.put('/api/modificar/:id', modificarUsuario);
 router.get('/api/listar', listarUsuarios);
+router.delete('/api/borrar/:id', borrarUsuario);
 
 
 
